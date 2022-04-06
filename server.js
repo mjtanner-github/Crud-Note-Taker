d61b28b43e92551aeb2e70dfaae1ab7c8cfe0d3f
Matthew Tanner <matthew.j.tanner@gmail.com>
2022-04-06 15:04:43 -0400

    Update server.js
    Changed listen port from 3001 to process.env.PORT || 3001;

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const notesDB = require('./db/db.json');
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.json());
@@ -83,6 +84,6 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.listen(3001, () =>
+app.listen(PORT, () =>
   console.log(`App listening at http://localhost:3001 🚀`)
 );
